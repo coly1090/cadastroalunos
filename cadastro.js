3f10d288dd255f56d78c9fe158ff983639614137
coly1090 <ferreira.emyly@escola.pr.gov.br>
2025-11-06 21:19:27 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,10 @@
+const primeironome="jose";
+const sobrenome="ferreira";
+let idade = 15;
+const nomecompleto=`${primeironome} ${sobrenome}`;
+console.log(`nomecompleto : ${nomecompleto}`);
+console.log(`idade do usuário :${idade}`);
+idade ++
+console.log(`aniversario do usuario: ${idade}`);
+nomecompleto = "ana souza"
+console.log(nomecompleto)
